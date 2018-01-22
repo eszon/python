d805bb4f05c7e554bffa5a8ff05202b00957e687
Igor - Work <igorlessav@gmail.com>
2018-01-22 11:04:42 -0200

    Melhorias

--- Projeto - Curso em video/challenges - mundo2/challenge-044.py
+++ Projeto - Curso em video/challenges - mundo2/challenge-044.py
@@ -1,41 +1,40 @@
 #/usr/bin/env python
-import os
-try:
-    valor = float(input('Digite o valor do produto: '))
+
+valor = float(input('Digite o valor do produto: '))
+
+f = int(input('FORMA DE PAGAMENTO:'
+              '\n [1] Á vista em dinheiro/cheque - 10% de desconto '
+              '\n [2] Á vista no cartão - 5% de desconto'
+              '\n [3] Em até 2x - Preço normal'
+              '\n [4] Em 3x ou mais no cartão - 20% de juros '
+              '\n Opção: '))
+
+d = valor - (valor * 10 / 100)
+d1 = valor - (valor * 5 / 100)
+j = valor + (valor * 20 / 100)
+
+if f == 1:
+    print(' Você escolheu pagamento à vista: R$ {:.2f}'.format(d))
+
+elif f == 2:
+    print('Você escolheu À vista no cartão: R${}'.format(d1))
+
+elif f == 3:
     parcelar = int(input('Parcelar: '))
-    if parcelar == 0:
-        pass
-    else:
-        p = valor / parcelar
-    f = int(input('FORMA DE PAGAMENTO:'
-                  '\n [1] Á vista em dinheiro/cheque - 10% de desconto '
-                  '\n [2] Á vista no cartão - 5% de desconto'
-                  '\n [3] Em até 2x - Preço normal'
-                  '\n [4] Em 3x ou mais no cartão - 20% de juros '
-                  '\n Opção: '))
-
-    if f == 1 and parcelar > 0:
-        print('Está opção é só para pagamento a vista.')
-        input('Pressione Enter para continuar.')
+    if parcelar != 2:
+        print('Parcelamento inválido, está opção é até em 2x. Por favor, tente novamente.')
         exit(1)
-    elif f == 2 and parcelar > 0:
-        print('Está opção é só para pagamento a vista.')
-        input('Pressione Enter para continuar.')
-        exit(1)
+    p = valor / parcelar
+    print('Você escolheu parcelar em {:.2f}x de R${:.2f}'.format(parcelar, p))
 
-    d = valor - (valor * 10 / 100)
-    d1 = valor - (valor * 5 / 100)
-    j = valor + (valor * 20 / 100)
+elif f == 4:
+    parcelar = int(input('Parcelar: '))
+    if parcelar > 10:
+        print('Desculpe, só é possível parcelar em até 10x. Por favor, tente novamente.')
+        exit()
+    p = valor / parcelar
+    print('Você escolheu parcelar em {}x vezes de R${:.2f}'.format(parcelar, p))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} com JUROS no final.'.format(valor, j))
 
-
-    if f == 1 and parcelar == 0:
-        print(' Você escolheu pagamento à vista: R$ {:.2f}'.format(d))
-    elif f == 2 and parcelar == 1:
-        print('Você escolheu À vista no cartão: R${}'.format(d1))
-    elif f == 3 and parcelar == 2:
-        print('Você escolheu parcelar em {}x de R${}'.format(parcelar, p))
-    else:
-        print('Você escolheu parcelar em {}x vezes de R${:.2f} totalizando R${:.2f}'.format(parcelar, p, j))
-
-except:
-    print('Opção invalida, procure o administrador de sistemas.')
+else:
+    print('Opção invalida. Por favor, tente novamente.')
