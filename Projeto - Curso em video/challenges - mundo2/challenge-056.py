b27aaf09b29489ccea3fd9edf554386cfc4bf9d6
Igor Vieira <igorlessav@gmail.com>
2018-02-06 21:50:02 -0200

    Exercicio 56-01

--- Projeto - Curso em video/challenges - mundo2/challenge-056.py
+++ Projeto - Curso em video/challenges - mundo2/challenge-056.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python
 
 # variaveis
-nome_lista = []
-idade_lista = []
-sexo_lista = []
 soma_idade = 0
 contador = 0
 contador2 = 0
+maior_idade = 0
+nome_maior_idade = 'vazio'
 
 # logica
-for i in range(1, 3):
+for i in range(1, 5):
     print(5 * '-', '{}º PESSOA'.format(i), 5 * '-')
 
     nome = str(input('Nome: '))
-    nome_lista.append(nome)
-
     idade = int(input('Idade: '))
-    idade_lista.append(idade)
-
-
     sexo = str(input('Sexo [M/F]: '))
-    sexo_lista.append(sexo)
 
-    maior_idade = max(idade_lista)
     soma_idade += idade
     contador += 1
 
+    if i == 1 and sexo in 'Mm':
+              maior_idade = idade
+              nome_maior_idade = nome
 
-if sexo == "F" and idade < 20:
+    if sexo in 'Mm' and idade > maior_idade:
+        maior_idade = idade
+        nome_maior_idade = nome
+
+if sexo in "Ff" and idade < 20:
     contador2 += 1
 
 media_idade_geral = soma_idade / contador
 
 # resultado
 print('A média de idade do grupo é {} anos'.format(media_idade_geral))
-print('O homem mais velho tem {} anos e se chama {}'.format(maior_idade, ))
+print('O homem mais velho tem {} anos e se chama {}'.format(maior_idade, nome_maior_idade))
 print('Ao todo são {} mulheres com menos de 20 anos'.format(contador2))
 
 
