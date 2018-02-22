01294eb7fd31a944a41f1fee8e6ccda13c27d377
Igor - Work <igorlessav@gmail.com>
2018-02-22 09:35:54 -0300

    Fix Call Function - Chatbot

--- Testes/Chat/Chatbot/Projeto - Mia/Class_PreProcess.py
+++ Testes/Chat/Chatbot/Projeto - Mia/Class_PreProcess.py
@@ -10,18 +10,18 @@ class PreProcessed(object):
     def fslice(self, param1):
         s = param1.split(' ')[1]
         print(s)
-        cpp = PreProcessed()
-        exec(cpp + s + f'{()}')
+        exec(f'self.{s}' + f'{()}')
 
 
     def logicfuc(self, param2):
+        #cpp = PreProcessed
         response2 = param2.split(' ')[0]
         if response2 == 'function':
             print('9Mia:', end=' ')
             self.fslice(param2)
 
         else:
-           print(f'{fhour()} Mia: ', param2, )
+           print(f'{self.ftime()} Mia: ', param2, )
 
     def helloworld(self):
         print('Fucking Hello World!')
