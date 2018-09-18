b10c2a4468d85c7ea9449bd8ef6adae103d251a3
Igor Vieira <igorlessav@gmail.com>
2018-09-18 05:44:43 -0300

    Update exec005

---
+++ Cursos/Curso de Python 3 - Canal Curso em Vídeo/challenges mundo1 + mundo2 - review/exec005.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor.
+
+num = int(input('Digite um número: '))
+
+print('O número digitado foi {}, seu antecessor é {} e seu sucessor é {}.'.format(num, (num - 1), (num + 1)))
+
