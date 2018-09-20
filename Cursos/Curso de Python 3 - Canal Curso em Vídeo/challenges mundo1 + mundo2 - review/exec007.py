81c7e3679b226bbcfc2718c1d650bcf327b4f7f2
Igor Vieira <igorlessav@gmail.com>
2018-09-20 05:33:49 -0300

    Refazendo Exercicios

---
+++ Cursos/Curso de Python 3 - Canal Curso em Vídeo/challenges mundo1 + mundo2 - review/exec007.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+n3 = float(input('Digite a terceira nota: '))
+
+media = (n1 + n2 + n3) / 3
+
+print(' A média entre {} {} {} é igual {:.2f}.'.format(n1, n2, n3, media))
