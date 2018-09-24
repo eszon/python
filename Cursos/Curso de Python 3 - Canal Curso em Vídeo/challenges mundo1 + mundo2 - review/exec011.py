1b54ec22e5dd869ba20d5c2031f79917db070bbb
Igor Vieira <igorlessav@gmail.com>
2018-09-24 05:29:21 -0300

    Refazendo Exec011

---
+++ Cursos/Curso de Python 3 - Canal Curso em Vídeo/challenges mundo1 + mundo2 - review/exec011.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+
+larg = float(input('Digite a largura em metros: '))
+alt = float(input('Digite a altura em metros: '))
+
+area = larg * alt
+
+tinta = area / 2
+
+print('Sua parede tem dimensão de {} x {} e sua área é de {}m².'.format(larg, alt, area ))
+print('Para pintar essa parede, você precisará  de {}l de tinta'.format(tinta))
