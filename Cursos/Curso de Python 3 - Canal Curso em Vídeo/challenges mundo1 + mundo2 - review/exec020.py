bb7139461f8ea9d44a6d4e413a98eeb9d88cf7d0
Igor Vieira <igorlessav@gmail.com>
2018-10-04 05:50:13 -0300

    Refazendo exec020

---
+++ Cursos/Curso de Python 3 - Canal Curso em Vídeo/challenges mundo1 + mundo2 - review/exec020.py
@@ -0,0 +1,13 @@
+# O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+from random import shuffle
+
+aluno1 = str(input('Primeiro Aluno: '))
+aluno2 = str(input('Segundo Aluno: '))
+aluno3 = str(input('Terceiro Aluno: '))
+aluno4 = str(input('Quarto Aluno: '))
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+
+shuffle(lista)
+
+print('A ordem de apresentação será: {}'.format(lista))
